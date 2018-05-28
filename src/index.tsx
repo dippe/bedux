ec9044add65260177456bb066670ccb9f493a3cc
Peter Dajka <peter_dajka@epam.com>
2018-05-28 23:05:19 +0200

    implement a simple functional table rendering with perf measurement

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,28 @@
 import * as React from 'react';
+import * as perf from 'react-addons-perf';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import { ELEMS, getRows, Table } from './tablegen';
 
-ReactDOM.render(
-  <App />,
+const renderApp = (props: any) => (ReactDOM.render(
+  <div>
+    <Table {...props}>
+      {ELEMS.map(getRows)}
+    </Table>
+    <hr />
+    <App />
+  </div>,
   document.getElementById('root') as HTMLElement
-);
+));
+
 registerServiceWorker();
+
+perf.start();
+renderApp({ text: 'elso' });
+renderApp({ text: 'elso' });
+renderApp({ text: 'elso' });
+perf.stop();
+perf.printWasted();
+perf.printExclusive();
